bb7a46d17ec4cdeb0d6646a22abd79161ba5739f
Niklas Terhoeven <niklas.terhoeven@analife.de>
2020-02-21 18:33:14 +0100

    clean up

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,11 @@
+var store = [{
+        "title": "Bilder In Blogposts",
+        "excerpt":"Dieser Post dient zum Testen. ich werde versuchen Bilder einzufügen:  Man sollte vor und nach dem Bild eine Zeile frei lassen, dann rendert Jekylldas Markdown so wie hier. ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/Bilder-in-Blogposts/",
+        "teaser":null},{
+        "title": "Testpost",
+        "excerpt":"This is a Blog post for testing the blogging stuff. We will write cool stuff here. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/testpost/",
+        "teaser":null}]
